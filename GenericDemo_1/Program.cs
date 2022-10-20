0557f1a51baadaa76c04af8c790674585b7471e5
Jay Pipaliya <113347625+yudiz-jay@users.noreply.github.com>
2022-10-20 11:03:29 +0530

    Add files via upload
    Those Demos of Generic Class, Array and Methods

---
+++ GenericDemo_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GenericDemo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DataStore<string> strStore = new DataStore<string>();
+
+            idValuePair<int, string> ivp1 = new idValuePair<int, string>();
+            ivp1.id = 1;
+            ivp1.Value = "Information Technology";
+            Console.WriteLine(ivp1.id + ", " + ivp1.Value);
+
+            idValuePair<string,string> ivp2 = new idValuePair<string, string>();
+            ivp2.id = "Jay";
+            ivp2.Value = "B.E. Computer Engineering";
+            Console.WriteLine(ivp2.id + ", " + ivp2.Value);
+        }
+    }
+
+    class DataStore <T>
+    {
+        public T Data { get; set; }
+    }
+
+    class idValuePair<Tid, TValue>
+    {
+        public Tid id { get; set; }
+        public TValue Value { get; set; }
+    }
+}
---
+++ GenericDemo_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GenericDemo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataStore<string> trainee = new DataStore<string>();
+
+            trainee.add(0,"Jay");
+            trainee.add(1,"Fayzan");
+            trainee.add(2,"Kenil");
+            trainee.add(3,"Smit");
+            trainee.add(4,"Mitesh");
+
+            Console.WriteLine(trainee.getData(2));
+
+            DataStore<string> city = new DataStore<string>();
+		    city.add(0, "Vadodara");
+		    city.add(1, "Rajkot");
+		    city.add(2, "Mumbai");
+
+            Console.WriteLine(city.getData(2));
+        }
+
+        class DataStore<T>
+        {
+            private T[] data = new T[10];
+
+            public void add(int id, T value)
+            {
+                if(id >= 0  && id < 10)
+                    data[id] = value;
+            }
+
+            public T getData(int id)
+            {
+                if(id >= 0  && id < 10)
+                    return data[id];
+                
+                else
+                    return default(T);
+            }
+        }
+
+
+    }
+}
